e7b24212a387ee57f79ee3b287d3f7f40aa50380
Lohayne Muriel Batista Bueno <47541289+LohayneMuriel@users.noreply.github.com>
2022-05-30 18:12:27 -0300

    Update es_t8a.c

--- es_t8a.c
+++ es_t8a.c
@@ -1,3 +1,10 @@
+/*Enunciado:	 Escreva	 um	 programa	 que	 (i)	 defina	 uma	 estrutura	 chamada	 “Ponto”,	 que	
+deverá	conter	as	coordenadas	x	e	y	de	um	ponto	qualquer	no	espaço	bidimensional (valores	
+reais),	(ii)	leia	as	coordenadas	de	dois	pontos	quaisquer	do	 teclado,	(iii)	calcule	a	distância	
+euclidiana	entre	eles	e	(iv)	diga	se	os	dois	pontos	são	iguais.	Como	você	estará	trabalhando	
+com pontos	flutuantes,	não	busque igualdades exatas:	considere	que	dois	pontos	são	iguais	
+se	a	distância	euclidiana	entre	eles	for	inferior	a	0,0000001.*/
+    
 #include<stdio.h>
 #include<math.h>
 
