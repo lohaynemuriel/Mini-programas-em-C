52325ae7eec9bd5aa5af4f39da7e9bb8b58c9601
Lohayne Muriel Batista Bueno <47541289+LohayneMuriel@users.noreply.github.com>
2022-06-02 12:55:54 -0300

    Update es_t9aa.c

--- es_t9aa.c
+++ es_t9aa.c
@@ -1,3 +1,7 @@
+/*Enunciado: Escreva um programa que receba o valor do raio de uma esfera e retorne o seu 
+volume. O cálculo do volume da esfera deve ser feito em uma função chamada 
+“volume_esfera” definida antes da função main().*/
+
 #include<stdio.h>
 #include<math.h>
 
