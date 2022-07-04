4c47666e2056bbf3d80d9a3ac8c74cdf40e1b6a2
Lohayne Muriel Batista Bueno <47541289+LohayneMuriel@users.noreply.github.com>
2022-07-04 18:40:53 -0300

    Add files via upload

---
+++ es_t10b.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int m,n, i,j, aux=0;
+    
+    scanf("%d", &m);
+    scanf("%d", &n);
+    
+    int** mat = (int**) malloc(m*sizeof(int*));
+
+    for(i=0; i<m; i++){
+        mat[i] = (int*) malloc(n*sizeof(int));
+    }
+    
+    if(mat == NULL)
+        return 1;
+        
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            scanf("%d", &mat[i][j]);}
+    }
+
+    if(m==n){
+        for(i=0; i<m; i++){
+            for(j=0; j<n; j++){
+                if(mat[i][j]==mat[j][i]){
+                    aux++;}}}
+    }
+    
+    if(aux != (m*n)){
+        printf("Nao eh simetrica\n");}
+    else{
+        printf("Eh simetrica\n");}
+        
+    for (i=0; i<m; i++)
+        free(mat[i]);
+    
+    free(mat);
+
+    return 0;
+}
